5696564207c78c1e621a73e20c0927b4ede9408d
jackhack2002 <jacobjabakumar2002@gmail.com>
2023-03-17 11:57:23 +0530

    update

--- app/src/main/java/com/jackhack2002/mychat/SplashScreen.java
+++ app/src/main/java/com/jackhack2002/mychat/SplashScreen.java
@@ -14,9 +14,9 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                startActivity(new Intent(SplashScreen.this,AuthActivity.class));
                 finish();
             }
-        },2500);
+        },1000);
     }
 }
